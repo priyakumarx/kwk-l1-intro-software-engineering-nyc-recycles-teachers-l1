b368f17b2f0bf37212f4c53a037ad02cc9657c52
Priya Kumar <priya.kumar08@yahoo.com>
2018-07-26 13:45:14 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -43,7 +43,8 @@ end
 #This method should print out each value in the hash.
 #You should use the `each_value` method to iterate over your hash and print out each value.
 
-def all_data.each_value do |data|
+def all_value
+  first_recycle_bin.each_value do |data|
     puts data
 end 
 end 

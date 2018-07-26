d4a3744b95ba0d1de43b9e3f571120a520d587a1
Priya Kumar <priya.kumar08@yahoo.com>
2018-07-26 13:47:25 +0000

    Done.

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -15,20 +15,47 @@
 # }
 
 
+
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
 
+def address(first_recycle_bin)
+  first_recycle_bin["address"]
+end 
+
 #2. Define a method called `borough` that takes a hash as an argument.
+
+def borough(first_recycle_bin)
+  first_recycle_bin["borough"]
+end 
 # This method should return the value of the borough key in the hash.
 
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash. 
 #You should use the `each_key` method to iterate over your hash and print out each key.
 
+def all_keys(first_recycle_bin)
+  first_recycle_bin.each_key do |info|
+    puts info 
+  end 
+end 
+
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
 #You should use the `each_value` method to iterate over your hash and print out each value.
 
+def all_data(first_recycle_bin)
+  first_recycle_bin.each_value do |data|
+    puts data
+end 
+end 
+
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
+
+def key_and_data(first_recycle_bin)
+  first_recycle_bin.each do |key, value|
+  puts "The #{key} is #{value}"
+end 
+end 
